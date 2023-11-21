91b95b2af00e502815da6259d7d4dd183c7abb92
Aven Bross <kieroda@gmail.com>
2023-11-21 17:11:19 +0000

    updated count example

--- examples/count.zig
+++ examples/count.zig
@@ -35,42 +35,30 @@ test "explicit implementation" {
     try testing.expectEqual(@as(usize, 10), count);
 }
 
+const MyCounter = struct {
+    count: usize,
+
+    pub fn increment(self: *@This()) void {
+        self.count += 1;
+    }
+
+    pub fn read(self: *const @This()) usize {
+        return self.count;
+    }
+};
+
 test "infer implementation" {
-    const MyCounter = struct {
-        count: usize,
-
-        pub fn increment(self: *@This()) void {
-            self.count += 1;
-        }
-
-        pub fn read(self: *const @This()) usize {
-            return self.count;
-        }
-    };
     var counter: MyCounter = .{ .count = 0 };
     countToTen(&counter, .{});
     try testing.expectEqual(@as(usize, 10), counter.count);
 }
 
+fn otherInc(self: *MyCounter) void {
+    self.count = 1 + self.count * 2;
+}
+
 test "override implementation" {
-    const MyCounter = struct {
-        count: usize,
-
-        pub fn increment(self: *@This()) void {
-            self.count += 1;
-        }
-
-        pub fn read(self: *const @This()) usize {
-            return self.count;
-        }
-    };
-
-    const S = struct {
-        pub fn incThree(self: *MyCounter) void {
-            self.count = 1 + self.count * 2;
-        }
-    };
     var counter: MyCounter = .{ .count = 0 };
-    countToTen(&counter, .{ .increment = S.incThree });
+    countToTen(&counter, .{ .increment = otherInc });
     try testing.expectEqual(@as(usize, 15), counter.count);
 }
